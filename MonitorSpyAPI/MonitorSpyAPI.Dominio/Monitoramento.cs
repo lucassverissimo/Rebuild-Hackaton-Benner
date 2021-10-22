f29aefaf7ae2e905381b1bbb31181379e9a2523e
Vinicius V <vvv1010@hotmail.com>
2021-10-22 16:29:19 -0300

    Commit parcial

---
+++ MonitorSpyAPI/MonitorSpyAPI.Dominio/Monitoramento.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MonitorSpyAPI.Dominio {
+    public class Monitoramento {
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<BlogPostsCount>()
+                .HasNoKey();
+        }
+
+        public string Token { get; set; }
+        public string Perfil { get; set; }
+        public string Usuario { get; set; }
+        public DateTime DataHora { get; set; }
+        public string Aplicacao { get; set; }
+        public string Acao { get; set; }
+        public string Modulo { get; set; }
+        public string Funcionalidade { get; set; }
+        public string Cliente { get; set; }
+    }
+}
