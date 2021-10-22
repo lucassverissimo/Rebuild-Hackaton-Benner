bf88ed04abd750ae63efbe41cb9884a934b1d59a
Vinicius V <vvv1010@hotmail.com>
2021-10-22 18:17:13 -0300

    Commit parcial

---
+++ MonitorSpyAPI/MonitorSpyAPI/Controllers/MonitoramentoController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MonitorSpyAPI.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MonitoramentoController : ControllerBase {
+        // GET: api/<MonitoramentoController>
+        [HttpGet]
+        public IEnumerable<string> Get() {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MonitoramentoController>/5
+        [HttpGet("{id}")]
+        public string Get(int id) {
+            return "value";
+        }
+
+        // POST api/<MonitoramentoController>
+        [HttpPost]
+        public void Post([FromBody] string value) {
+        }
+
+        // PUT api/<MonitoramentoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) {
+        }
+
+        // DELETE api/<MonitoramentoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id) {
+        }
+    }
+}
